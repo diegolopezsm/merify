dac36232ad9391623aa2bde8a8c493098de5ab17
Diego.Lopez <diego.lopez@MacBookAir-DiegoLopez.local>
2025-10-10 17:55:14 -0500

    feat(0.2.0)

---
+++ src/shared/components/icons/PlugIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="w-4 h-4 mr-2"
+  >
+    <path d="M12 22v-5"></path>
+    <path d="M9 8V2"></path>
+    <path d="M15 8V2"></path>
+    <path d="M18 8v5a4 4 0 0 1-4 4h-4a4 4 0 0 1-4-4V8Z"></path>
+  </svg>
+</template>
